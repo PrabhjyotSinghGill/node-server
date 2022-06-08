308efa1bca45faa3d208acbcded542d2c3fec8c1
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-06-07 23:55:56 -0400

    Handling HTTP POST Requests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.json());
+
 
 const courses = [
   { id: 1, name: "Digital Electronics" },
@@ -30,6 +32,15 @@ app.get("/courses", (req, res) => {
 app.get("/courses/:id", (req, res) => {
   const course = courses.find((val) => val.id === parseInt(req.params.id));
   if (!course) res.status(404).send("Not found!"); // response 404
+  res.send(course);
+});
+
+app.post('/courses',(req,res)=>{
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name 
+  };
+  courses.push(course);
   res.send(course);
 });
 
