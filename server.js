de642ccfd784fb6700e074c19582c9e001fff389
PrabhjyotSinghGill <prabhjyotg@gmail.com>
2022-06-07 23:33:31 -0400

    Handling HTTP GET Requests

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome')
-})
+const courses = [
+  { id: 1, name: "Digital Electronics" },
+  { id: 2, name: "Data Structures" },
+  { id: 3, name: "Compiler" },
+  { id: 4, name: "Algorithms" },
+  { id: 5, name: "Theory of Computation" },
+  { id: 6, name: "Computer Architecture" },
+];
 
-app.get('/echo/:para', (req, res) => {
-    res.send('Echo! '+req.params.para+' '+[1,2,3])
-  })
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
-app.get('/dob/:date/:month/:year', (req, res) => {
-    res.send(req.params)
-})
+app.get("/echo/:para", (req, res) => {
+  res.send("Echo! " + req.params.para);
+});
+
+app.get("/dob/:date/:month/:year", (req, res) => {
+  res.send(req.params);
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const course = courses.find((val) => val.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Not found!"); // response 404
+  res.send(course);
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
